2891c34a98327aeb149e075ea2c1b1ee7f34df4f
SvelteRust <oddharald@myhren.ai>
2025-08-11 06:51:50 +0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import "./pico.css";
+
+  // Props
+  const { children } = $props();
+</script>
+
+<main class="container">
+  {@render children()}
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+  <title>Minimal SaaS</title>
+</svelte:head>
+
+<h1>Minimal SaaS</h1>
+<p>Welcome to my simple, blazingly fast web app.</p>
+
+<form>
+  <div class="grid">
+    <label>
+      First name
+      <input name="name" placeholder="Name" />
+    </label>
+    <label>
+      Email
+      <input name="email" placeholder="Email" type="email" />
+    </label>
+  </div>
+
+  <button type="submit">Subscribe</button>
+</form>
